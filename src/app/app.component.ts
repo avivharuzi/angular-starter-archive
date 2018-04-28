4d4429cba61d2c80678d5ead0433e2025c08e9eb
avivharuzi <avivharuzi@gmail.com>
2018-04-28 10:17:28 +0300

    Added bootstrap scss files and more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/mergeMap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit  {
   constructor(
