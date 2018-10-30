e747a45db54e84e7921d13b41b0335f48108e2b6
avivharuzi <avivharuzi@gmail.com>
2018-10-30 11:07:42 +0200

    Update urls

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -1,4 +1,6 @@
-const BASE_URL: string = 'http://localhost:8080';
+import { environment } from './../../environments/environment.staging';
+
+const BASE_URL: string = environment.production ? '' : 'http://localhost:8080';
 
 export const DEFAULT_IMAGE_PATH: string = 'assets/images/defaults/default-image.png';
 export const LOADING_GIF_PATH: string = 'assets/images/tools/loading.gif';
