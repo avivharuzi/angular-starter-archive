f99db44262e5f5d8a8b379d68036446843582427
Aviv Haruzi <avivharuzi@gmail.com>
2020-04-19 21:51:18 +0300

    Fix app component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
+import { NgxSeoModule } from '@avivharuzi/ngx-seo';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, NgxSeoModule.forRoot()],
       declarations: [AppComponent],
     }).compileComponents();
   }));
