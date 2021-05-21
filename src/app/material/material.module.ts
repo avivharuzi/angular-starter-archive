23eb6fa0aaa631fa3c5b159cb3f18b87f2dd6d85
Aviv Haruzi <avivharuzi@gmail.com>
2021-05-21 18:43:20 +0300

    Update angular material version

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [],
   exports: [],
 })
 export class MaterialModule {}
